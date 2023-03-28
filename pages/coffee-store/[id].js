39df68feb7c155f38a9c1ff83995d766cdc78a67
okazakee <cristiandcbusiness@gmail.com>
2023-03-28 12:54:48 +0200

    lesson 41

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,11 +1,16 @@
 import { useRouter } from 'next/router';
-import React from 'react'
+import Link from 'next/link';
 
 const CoffeeStore = () => {
   const router = useRouter();
 
   return (
-    <div>Coffee Store Page {router.query.id}</div>
+    <div>
+      Coffee Store Page {router.query.id}
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
+    </div>
   )
 }
 
