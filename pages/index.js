aa6bf4afbd730d5a9cc03447754b8d1d4127971e
okazakee <cristiandcbusiness@gmail.com>
2023-03-28 11:39:45 +0200

    lesson 35

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Banner from "../components/banner";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  console.log("styles", styles);
+  const handleOnBannerClick = () => {
+    console.log("ats");
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,9 +18,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Coffee Connoisseur</h1>
+        <Banner buttonText="View stores nearby" handleOnClick={handleOnBannerClick}></Banner>
       </main>
-
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
