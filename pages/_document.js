a775ade532af1bcbf7adc120a43f1ab4996ce2cf
okazakee <cristiandcbusiness@gmail.com>
2023-03-28 16:42:07 +0200

    lesson 56

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { NextScript, Html, Head, Main } from "next/document"
+
+class MyDocument extends Document {
+    render() {
+        return <Html>
+            <Head>
+                <link rel="preload" href="/fonts/IBMPlexSans-Bold.ttf" as="font" crossOrigin="anonymous" />
+                <link rel="preload" href="/fonts/IBMPlexSans-Regular.ttf" as="font" crossOrigin="anonymous" />
+                <link rel="preload" href="/fonts/IBMPlexSans-SemiBold.ttf" as="font" crossOrigin="anonymous" />
+            </Head>
+            <body>
+                <Main></Main>
+                <NextScript />
+            </body>
+        </Html>
+    }
+}
+
+export default MyDocument;
