a475d4a227694b57bfa15a6150a4292256c6b119
okazakee <cristiandcbusiness@gmail.com>
2023-05-28 19:02:59 +0200

    lesson 121

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
+import { createContext } from "react/cjs/react.production.min";
 import "../styles/globals.css";
+
+const StoreContext = createContext();
+
+const StoreProvider = ({children}) => {
+  const initialState = {
+    latLong: "",
+    coffeeStores: []
+  };
+
+  <StoreContext.Provider value={{state: initialState}}>
+  {children}
+  </StoreContext.Provider>
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <Component {...pageProps} />
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
     </div>
   );
 }
