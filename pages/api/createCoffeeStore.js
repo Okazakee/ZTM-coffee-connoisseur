86cfc9bcedcb7748ac69593c819af818247d3e92
okazakee <cristiandcbusiness@gmail.com>
2023-06-07 15:24:44 +0200

    lesson 149

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -5,56 +5,63 @@ const base = new Airtable({ apiKey: process.env.AIRTABLE_API_TOKEN }).base(
 const table = base("coffee-stores");
 
 const createCoffeeStore = async (req, res) => {
-  if (req.method === "POST") {
+    if (req.method === "POST") {
+    const {id, name, address, imgUrl, voting} = req.body;
 
-    try {
-        //find a record
+        try {
+            //find a record
+            if (id) {
+                const findCoffeeStoreRecords = await table
+                .select({
+                    filterByFormula: `id=${id}`,
+                })
+                .firstPage();
 
-        const {id, name, address, imgUrl, voting} = req.body;
+                if (findCoffeeStoreRecords.length !== 0) {
 
-        const findCoffeeStoreRecords = await table
-          .select({
-            filterByFormula: `id=${id}`,
-          })
-          .firstPage();
+                    const records = findCoffeeStoreRecords.map((record) => {
+                        return {
+                            ...record.fields
+                        }
+                    });
 
-        if (findCoffeeStoreRecords.length !== 0) {
+                    res.json({message: "Finded record", records});
+                } else {
 
-            const records = findCoffeeStoreRecords.map((record) => {
-                return {
-                    ...record.fields
+                //create a record
+                if (name) {
+
+                    const createCoffeeStoreRecords = await table.create([
+                        {
+                            fields: {
+                            id,
+                            imgUrl,
+                            name,
+                            address,
+                            voting
+                            }
+                        }
+                    ]);
+
+                    const records = createCoffeeStoreRecords.map((record) => {
+                        return {
+                            ...record.fields
+                        }
+                    });
+
+                    res.json({message: "Created new record", records});
+                } else {
+                    res.status(400);
+                    res.json({message: "Name is missing!"});
                 }
-            });
-
-            res.json({message: "Finded record", records});
+            }
         } else {
-            //create a record
-
-            const createCoffeeStoreRecords = await table.create([
-                {
-                    fields: {
-                    id,
-                    imgUrl,
-                    name,
-                    address,
-                    voting
-                    }
-                }
-            ]);
-
-            const records = createCoffeeStoreRecords.map((record) => {
-                return {
-                    ...record.fields
-                }
-            });
-
-            res.json({message: "Created new record", records});
+            res.status(400);
+            res.json({message: "ID is missing!"});
         }
-      }
-      catch (error) {
-        console.error('error finding store: ', error);
-        res.json({message: 'error finding store: ', error});
+    } catch (error) {
         res.status(500);
+        res.json({message: 'Error finding/creating store:', error});
       }
     }
 };
