8cb26917ac1939c3eca0a2633f90feafe90502e9
okazakee <cristiandcbusiness@gmail.com>
2023-06-07 14:21:37 +0200

    lesson 146

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -7,24 +7,34 @@ const table = base("coffee-stores");
 const createCoffeeStore = async (req, res) => {
   if (req.method === "POST") {
 
-    //find a record
-    const findCoffeeStoreRecords = await table
-      .select({
-        filterByFormula: `id="0"`,
-      })
-      .firstPage();
-
-    console.log({ findCoffeeStoreRecords });
-
-    if (findCoffeeStoreRecords.length !== 0) {
-      res.json(findCoffeeStoreRecords);
-    } else {
-      //create a record
-      res.json({ message: "create a record" });
+    try {
+        //find a record
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id="0"`,
+          })
+          .firstPage();
+    
+        if (findCoffeeStoreRecords.length !== 0) {
+    
+            const records = findCoffeeStoreRecords.map((record) => {
+                return {
+                    ...record.fields
+                }
+            });
+    
+            res.json(records);
+        } else {
+          //create a record
+          res.json({ message: "create a record" });
+        }
+      }
+      catch (error) {
+        console.error('error finding store: ', error);
+        res.json({message: 'error finding store: ', error});
+        res.status(500);
+      }
     }
-  } else {
-    return res.json({message: "change method dumbass"})
-  }
 };
 
 export default createCoffeeStore;
