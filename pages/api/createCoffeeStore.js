7b2a5e404713c1d2e9e6f039dc4d15f1bacc06f7
okazakee <cristiandcbusiness@gmail.com>
2023-06-07 13:06:16 +0200

    lesson 145

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,14 +1,30 @@
-const Airtable = require('airtable');
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_TOKEN}).base(process.env.AIRTABLE_BASE_KEY);
-
+const Airtable = require("airtable");
+const base = new Airtable({ apiKey: process.env.AIRTABLE_API_TOKEN }).base(
+  process.env.AIRTABLE_BASE_KEY
+);
 const table = base("coffee-stores");
 
-const createCoffeeStore = (req, res) => {
-    if (req.method === "POST") {
-        res.json({table: table.name})
+const createCoffeeStore = async (req, res) => {
+  if (req.method === "POST") {
+
+    //find a record
+    const findCoffeeStoreRecords = await table
+      .select({
+        filterByFormula: `id="0"`,
+      })
+      .firstPage();
+
+    console.log({ findCoffeeStoreRecords });
+
+    if (findCoffeeStoreRecords.length !== 0) {
+      res.json(findCoffeeStoreRecords);
     } else {
-        res.json({message: "Method not allowed! Use POST."})
+      //create a record
+      res.json({ message: "create a record" });
     }
+  } else {
+    return res.json({message: "change method dumbass"})
+  }
 };
 
 export default createCoffeeStore;
